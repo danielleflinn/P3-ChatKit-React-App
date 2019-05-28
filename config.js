7ea4f05cf8ae534882db7f02258713934168e7ef
Danielle Flinn <danielleflinn29@gmail.com>
2019-05-28 12:16:12 -0700

    Added files

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/5dadacdf-c15b-4f4a-bb67-ec6deec116e6/token";
+const instanceLocator = "v1:us1:5dadacdf-c15b-4f4a-bb67-ec6deec116e6";
+
+export { tokenUrl, instanceLocator }
