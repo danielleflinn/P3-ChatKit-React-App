84a52763e5044c97646e1b2cf30418b8e2110a09
Danielle Flinn <43477473+danielleflinn@users.noreply.github.com>
2019-06-03 13:09:48 -0700

    fixed the username error

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     componentDidMount() {
         const chatManager = new Chatkit.ChatManager({
             instanceLocator,
-            userId: 'emilycone',
+            userId: 'daniflinn',
             tokenProvider: new Chatkit.TokenProvider({
                 url: tokenUrl
             })
